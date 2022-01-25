a55367a283ffa03bfe54ba2dec1b3c2b2bfa1659
$ <hungover.hedgehogs@gmail.com>
2022-01-25 18:46:57 +1100

    domain change

--- src/components/Bg.js
+++ src/components/Bg.js
@@ -1,11 +1,12 @@
 import React from 'react';
 import { Image, Container } from 'react-bootstrap';
+import bg_1 from '../images/sewer_concept.png';
 
 const Bg = () => {
     return (
         <>
             <Container>
-                <Image variant="top" style={{ minHeight: '90vh', marginBottom: "12vh" }} src="holder.js/100px180?theme=industrial" />
+                <Image variant="top" style={{ maxWidth: "100%", marginBottom: "12vh" }} src={bg_1} />
             </Container>
         </>
     )
